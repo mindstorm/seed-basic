0b368112951ff2bf553add0311dd1df217c472a7
Paul Cervenka <paul.cervenka@gmail.com>
2016-04-11 11:02:43 +0200

    beautify

--- _dev/gulpfile.js
+++ _dev/gulpfile.js
@@ -43,11 +43,11 @@ var development = environments.development;
 /* get packackage.json
  * ------------------------------------------------ */
 var pkg;
-var getFile = function (file) {
+var getFile = function(file) {
   "use strict";
   return JSON.parse(fs.readFileSync(file));
 };
-var getConfig = function () {
+var getConfig = function() {
   "use strict";
 
   // get standard package.json
@@ -63,7 +63,7 @@ var getConfig = function () {
 
 /* replace tokens
  * ------------------------------------------------ */
-var replaceTokens = function (match, tokenName) {
+var replaceTokens = function(match, tokenName) {
   "use strict";
 
   var tokenValue = pkg[tokenName];
@@ -135,7 +135,7 @@ var config = {
 
 /* replace
  * ------------------------------------------------ */
-gulp.task("replace", ["package"], function (done) {
+gulp.task("replace", ["package"], function(done) {
   "use strict";
 
   gulp.src(config.replace.src)
@@ -187,7 +187,7 @@ gulp.task("replace", ["package"], function (done) {
 
 /* styles
  * ------------------------------------------------ */
-gulp.task("styles", function (done) {
+gulp.task("styles", function(done) {
   "use strict";
 
   gulp.src(config.styles.src)
@@ -201,7 +201,8 @@ gulp.task("styles", function (done) {
   }))
 
   // compile sass
-  .pipe(css_sass().on("error", css_sass.logError))
+  .pipe(css_sass()
+    .on("error", css_sass.logError))
 
   // add vendor prefixes
   .pipe(css_prefix({
@@ -243,7 +244,7 @@ gulp.task("styles", function (done) {
 
 /* scripts
  * ------------------------------------------------ */
-gulp.task("scripts", function (done) {
+gulp.task("scripts", function(done) {
   "use strict";
 
   gulp.src(config.scripts.src)
@@ -280,7 +281,7 @@ gulp.task("scripts", function (done) {
 
 /* concat all vendor specific styles
  * ------------------------------------------------ */
-gulp.task("vendor:styles", function (done) {
+gulp.task("vendor:styles", function(done) {
   "use strict";
 
   gulp.src(config.styles.vendor.src)
@@ -301,7 +302,7 @@ gulp.task("vendor:styles", function (done) {
 
 /* concat all vendor specific scripts
  * ------------------------------------------------ */
-gulp.task("vendor:scripts", function (done) {
+gulp.task("vendor:scripts", function(done) {
   "use strict";
 
   gulp.src(config.scripts.vendor.src)
@@ -322,7 +323,7 @@ gulp.task("vendor:scripts", function (done) {
 
 /* local webserver
  * ------------------------------------------------ */
-gulp.task("serve", function () {
+gulp.task("serve", function() {
   "use strict";
 
   serve({
@@ -336,7 +337,7 @@ gulp.task("serve", function () {
 
 /* get package.json combined with environment json
  * ------------------------------------------------ */
-gulp.task("package", function () {
+gulp.task("package", function() {
   "use strict";
 
   pkg = getConfig();
@@ -355,7 +356,7 @@ gulp.task("build", ["replace", "styles", "scripts", "vendor:styles", "vendor:scr
 
 /* watch files for changes
  * ------------------------------------------------ */
-gulp.task("watch", ["build"], function () {
+gulp.task("watch", ["build"], function() {
   "use strict";
 
   // watch and reload browsersync
--- _dev/scripts/main.js
+++ _dev/scripts/main.js
@@ -1,9 +1,9 @@
 /*
  * JQUERY - DOCUMENT READY
  ****************************************************/
-$(function () {
+
+$(function() {
   "use strict";
 
   console.log("Hello World!");
-
 });
